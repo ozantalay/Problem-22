1f29cdffb7503f6d5f3a7850fd248f242e8dfc99
ozantalay <ozntalay@hotmail.com>
2024-09-04 22:13:10 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 export default function VideoPlayer() {
   const ref = useRef()
+  const [isPlaying,setIsPlaying]=useState(false)
 
   console.log(ref.current)
+
+  const handlePlay=()=>{
+    if(ref.current && ref.current.paused){
+      ref.current.play()
+      setIsPlaying(true)  
+  }
+  
+  }
+  const handlePause=()=>{
+    if(ref.current && !ref.current.paused){
+      ref.current.pause()
+      setIsPlaying(false)
+    }
+  }
 
   return (
     <div className='max-w-[500px] space-y-4 p-8 mx-auto'>
@@ -11,13 +26,14 @@ export default function VideoPlayer() {
         Video Oynatıcı
       </h1>
       <video
+        ref={ref}
         src='https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4'
         poster='https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217'
       />
       <div className='flex justify-between'>
-        <button className='text-orange-500 font-semibold'>Duraklat</button>
-        <div>Duraklatıldı</div>
-        <button className='text-indigo-500 font-semibold'>Oynat</button>
+        <button onClick={handlePause} className='text-orange-500 font-semibold' >Duraklat</button>
+        <div>{isPlaying?'oynatılıyor':'duraklatıldı'}</div>
+        <button onClick={handlePlay} className='text-indigo-500 font-semibold'>Oynat</button>
       </div>
     </div>
   )
